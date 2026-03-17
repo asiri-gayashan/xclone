6e309aa8701dcddc6bdf61377771e346d3571682
asiri-gayashan <gayashanx@gmail.com>
2026-03-17 14:24:54 +0530

    feat(routes): implement user profile retrieval and update functionality with authentication

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,18 @@
 import express from "express";
+import { getUserProfile, updateProfile  } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+
+
+router.get("/profile/:username", getUserProfile);
+router.put("/profile", protectRoute, updateProfile);
+
+
+
 export default router;
+
+
+
+
