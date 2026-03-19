02559b62fa7489d2838794ca058520c7cc592e09
asiri-gayashan <gayashanx@gmail.com>
2026-03-19 17:24:10 +0530

    feat(auth): implement social authentication and add Home component

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -4,9 +4,9 @@ import { Redirect, Stack } from 'expo-router'
 export default function AuthRoutesLayout() {
   const { isSignedIn, isLoaded } = useAuth()
 
-  if (!isLoaded) {
-    return null
-  }
+  // if (!isLoaded) {
+  //   return null
+  // }
 
   if (isSignedIn) {
     return <Redirect href={'/'} />
--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,6 +1,9 @@
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { useSocialAuth } from "@/hooks/useSocialAuth";
+import { ActivityIndicator, Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { handleSocialAuth, isLoading } = useSocialAuth();
+
   return (
     <View className="flex-1 bg-white">
       <View className="flex-1 px-8 justify-between">
@@ -13,11 +16,13 @@ export default function Index() {
               resizeMode="contain"
             />
           </View>
+
           <View className="flex-col gap-2">
+            {/* GOOGLE SIGNIN BTN */}
             <TouchableOpacity
               className="flex-row items-center justify-center bg-white border border-gray-300 rounded-full py-3 px-6"
-              // onPress={() => handleSocialAuth("oauth_google")}
-              // disabled={isLoading}
+              onPress={() => handleSocialAuth("oauth_google")}
+              disabled={isLoading}
               style={{
                 shadowColor: "#000",
                 shadowOffset: { width: 0, height: 1 },
@@ -26,22 +31,25 @@ export default function Index() {
                 elevation: 2,
               }}
             >
-              <View className="flex-row items-center justify-center">
-                <Image
-                  source={require("../../assets/images/google.png")}
-                  className="size-10 mr-3"
-                  resizeMode="contain"
-                />
-                <Text className="text-black font-medium text-base">
-                  Continue with Google
-                </Text>
-              </View>
+              {isLoading ? (
+                <ActivityIndicator size="small" color="#4285F4" />
+              ) : (
+                <View className="flex-row items-center justify-center">
+                  <Image
+                    source={require("../../assets/images/google.png")}
+                    className="size-10 mr-3"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-black font-medium text-base">Continue with Google</Text>
+                </View>
+              )}
             </TouchableOpacity>
 
+            {/* APPLE SIGNIN ICON */}
             <TouchableOpacity
               className="flex-row items-center justify-center bg-white border border-gray-300 rounded-full py-3 px-6"
-              // onPress={() => handleSocialAuth("oauth_apple")}
-              // disabled={isLoading}
+              onPress={() => handleSocialAuth("oauth_apple")}
+              disabled={isLoading}
               style={{
                 shadowColor: "#000",
                 shadowOffset: { width: 0, height: 1 },
@@ -50,33 +58,29 @@ export default function Index() {
                 elevation: 2,
               }}
             >
-              {/* {isLoading ? (
+              {isLoading ? (
                 <ActivityIndicator size="small" color="#000" />
-              ) : ( */}
-              <View className="flex-row items-center justify-center">
-                <Image
-                  source={require("../../assets/images/apple.png")}
-                  className="size-8 mr-3"
-                  resizeMode="contain"
-                />
-                <Text className="text-black font-medium text-base">
-                  Continue with Apple
-                </Text>
-              </View>
-              {/* )} */}
+              ) : (
+                <View className="flex-row items-center justify-center">
+                  <Image
+                    source={require("../../assets/images/apple.png")}
+                    className="size-8 mr-3"
+                    resizeMode="contain"
+                  />
+                  <Text className="text-black font-medium text-base">Continue with Apple</Text>
+                </View>
+              )}
             </TouchableOpacity>
           </View>
 
+          {/* Terms and Privacy */}
           <Text className="text-center text-gray-500 text-xs leading-4 mt-6 px-2">
-            By signing up, you agree to our{" "}
-            <Text className="text-blue-500">Terms</Text>
+            By signing up, you agree to our <Text className="text-blue-500">Terms</Text>
             {", "}
             <Text className="text-blue-500">Privacy Policy</Text>
             {", and "}
             <Text className="text-blue-500">Cookie Use</Text>.
           </Text>
-
-          
         </View>
       </View>
     </View>
