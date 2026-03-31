6d3570bc7b3c26fb03ed5b3cf63eeffcd92d9d82
asiri-gayashan <gayashanx@gmail.com>
2026-03-31 16:43:59 +0530

    fix: uncomment usePosts hook and related functionality in HomeScreen

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import PostComposer from "@/components/PostComposer";
 import PostsList from "@/components/PostsList";
 import SignOutButton from "@/components/SignOutButton";
-// import { usePosts } from "@/hooks/usePosts";
+import { usePosts } from "@/hooks/usePosts";
 import { useUserSync } from "@/hooks/useUserSync";
 import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
@@ -9,15 +9,15 @@ import { RefreshControl, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
-  // const [isRefetching, setIsRefetching] = useState(false);
-  // const { refetch: refetchPosts } = usePosts();
+  const [isRefetching, setIsRefetching] = useState(false);
+  const { refetch: refetchPosts } = usePosts();
 
-  // const handlePullToRefresh = async () => {
-  //   setIsRefetching(true);
+  const handlePullToRefresh = async () => {
+    setIsRefetching(true);
 
-  //   await refetchPosts();
-  //   setIsRefetching(false);
-  // };
+    await refetchPosts();
+    setIsRefetching(false);
+  };
 
   useUserSync();
 
@@ -33,13 +33,13 @@ const HomeScreen = () => {
         showsVerticalScrollIndicator={false}
         className="flex-1"
         contentContainerStyle={{ paddingBottom: 80 }}
-        // refreshControl={
-        //   <RefreshControl
-        //     refreshing={isRefetching}
-        //     onRefresh={handlePullToRefresh}
-        //     tintColor={"#1DA1F2"}
-        //   />
-        // }
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefetching}
+            onRefresh={handlePullToRefresh}
+            tintColor={"#1DA1F2"}
+          />
+        }
       >
         <PostComposer />
         <PostsList />
